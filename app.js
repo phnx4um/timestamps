9fef0b56f8ddc8486e8be099f33b57bf80e37f29
Nitin Verma <phnx4um@gmail.com>
2020-07-20 00:20:41 +0530

    display ts and labels UI elemets which are covered

--- app.js
+++ app.js
@@ -7,20 +7,47 @@ window.addEventListener('load', function() {
     console.log("hello");
     let holder = document.getElementById("holder");
     holder.addEventListener("mouseenter", e => {
-        divContainer.style.visibility = "visible";
+        // get this from youtube
+        let currentTime = "500"; // in seconds
+        let currentTS;
+        console.log(timeStampsInSeconds)
+        for (let i = 0; i < timeStampsInSeconds.length; i++) {
+            if (timeStampsInSeconds[i] >= currentTime) {
+                console.log(i);
+                currentTS = i;
+                break;
+            }
+        }
+        // now chage UI color to display which topics are done
+        for (let i = 0; i < currentTS; i++) {
+            if (i == (currentTS - 1)) {
+                console.log(i);
+                // this topic is still going on
+                let tsid = "ts" + i.toString();
+                let labeltextid = "labeltext" + i.toString();
+                document.getElementById(labeltextid).style.backgroundColor = "red";
+                break;
+            }
+            let labelid = "ts" + i.toString();
+            document.getElementById(labelid).style.backgroundColor = "#FF7F50";
+        }
+
+        // divContainer.style.visibility = "visible";
+
         // reset visibility to hidden
-        setTimeout(function() {
-            divContainer.style.visibility = "hidden";
-        }, 2000);
+        // setTimeout(function() {
+        //     divContainer.style.visibility = "hidden";
+        // }, 2000);
     });
 
+    // we will be getting this from textarea
     let string = `00:00:00  Introduction
-                          00:00:15  Uncertainty
-    00:04:52  Probability
-    00:09:37  Conditional Probability
-    00:17:19  Random Variables`
+                    00:00:15  Uncertainty
+                    00:04:52  Probability
+                    00:09:37  Conditional Probability
+                    00:17:19  Random Variables`
 
-    let currentTime = "500"; // in seconds
+
     let totalTime = "00:20:00";
 
     // this is what I will get as USER input after parsing regex
@@ -35,7 +62,7 @@ window.addEventListener('load', function() {
         var seconds = parseInt(a[0], 10) * 60 * 60 + parseInt(a[1], 10) * 60 + parseInt(a[2], 10);
         return seconds
     });
-    console.log(timeStampsInSeconds);
+    // console.log(timeStampsInSeconds);
     let timeStampRatios = []
     for (let i = 0; i < timeStampsInSeconds.length - 1; ++i) {
         timeStampRatios[i] = (timeStampsInSeconds[i + 1] - timeStampsInSeconds[i]) / timeStampsInSeconds[timeStampsInSeconds.length - 1];
@@ -45,7 +72,7 @@ window.addEventListener('load', function() {
     // generate UI
     let divContainer = document.createElement("div");
     divContainer.style.height = "80%";
-    divContainer.style.visibility = "hidden";
+    // divContainer.style.visibility = "hidden";
     divContainer.id = "container";
 
     let timeStampContainer = document.createElement("div");
@@ -63,13 +90,14 @@ window.addEventListener('load', function() {
         // create labels
         let labelDiv = document.createElement("div");
         labelDiv.style.height = height;
-        labelDiv.className = "label"
-        labelDiv.innerHTML = '<span class="labeltext">' + label + '</span>';
+        labelDiv.className = "label";
+        labelDiv.innerHTML = '<span class="labeltext" id=labeltext' + index.toString() + '>' + label + '</span>';
 
         // create timestamp UI
         let timeStampUI = document.createElement("div")
         timeStampUI.className = "timestamp";
         timeStampUI.style.width = "5px";
+        timeStampUI.id = "ts" + index.toString();
         // console.log(divContainer.offsetHeight);
         timeStampUI.style.height = height;
         // timeStampUI.style.backgroundColor = random_bg_color();
