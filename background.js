505f048f3b9f55c502298c672b53a58b7aadbb4e
Nitin Verma <phnx4um@gmail.com>
2020-07-30 21:10:58 +0530

    added firebase database to my project

--- background.js
+++ background.js
@@ -1,4 +1,21 @@
+// TODO: Replace the following with your app's Firebase project configuration
 let regWatch = /^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/watch\?+/gm;
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDoOvv_JnPMGjl2yrVGvCOEDT-mIb1iNeY",
+    authDomain: "time-line-a2ea3.firebaseapp.com",
+    databaseURL: "https://time-line-a2ea3.firebaseio.com",
+    projectId: "time-line-a2ea3",
+    storageBucket: "time-line-a2ea3.appspot.com",
+    messagingSenderId: "202568583915",
+    appId: "1:202568583915:web:2e5fcbedc8706ed3bb8cae",
+    measurementId: "G-K8LPNLPBHB"
+};
+
+firebase.initializeApp(firebaseConfig);
+console.log(firebase)
+
 
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
     console.log(details);
